fa0ecb5cea308bb3424fde719e7859ed0ad5cf91
NjeriEd <njeribekah@gmail.com>
2022-09-20 23:33:31 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 #include "2-strlen.c"
 
@@ -17,7 +18,7 @@ void puts_half(char *str)
 	}
 	for (i = (_strlen(str) + oE) / 2; i < _strlen(str); i++)
 	{
-		_putchar(str[i]);
+		putchar(str[i]);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
